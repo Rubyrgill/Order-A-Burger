e2fce016a0e9aa2de14df0710ea3a6c9ec93384b
RubyG <rubyrgill@hotmail.com>
2018-09-17 19:38:38 -0400

    initial server set up, connected

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("Hamburger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Veggie Burger", FALSE);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Chicken Burger", FALSE);
---
+++ server.js
@@ -0,0 +1,30 @@
+//dependencies
+var express = require('express');
+var bodyParser = require('body-parser');
+var exphbs = require('express-handlebars');
+var methodOverride = require('method-override');
+
+var app = express();
+var PORT = process.env.PORT || 6000;
+
+app.use(express.static(__dirname + '/public'));
+
+app.use(bodyParser.urlencoded({
+    extended: false
+}));
+
+app.use(methodOverride('_method'));
+app.engine('handlebars', exphbs({
+    defaultLayout: 'main'
+}));
+
+app.set('view engine', 'handlebars');
+
+var routes = require('./controllers/burgers_controller.js');
+
+app.use(routes);
+
+
+app.listen(PORT, function () {
+    console.log("Server listening  on: http://localhost:" + PORT);
+});
