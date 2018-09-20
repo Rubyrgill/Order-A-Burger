76ba13402ee180749259e9e6b8bbd81b766a567b
RubyG <rubyrgill@hotmail.com>
2018-09-19 20:23:07 -0400

    Connected database, displays in terminal

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -1,10 +1,14 @@
 var express = require('express');
-
 var router = express.Router();
+var burger = require("../models/burger.js")
 
 //linking to index.handlebars content
 router.get("/", function (req, res) {
-    res.render('./index');
+    burger.all(function (burger_data) {
+        console.log(burger_data);
+        res.render('./index');
+    })
+
 })
 
 module.exports = router;
